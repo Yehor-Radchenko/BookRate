e2a804dc4ac8bc3d5e3cb7eda944ccbff38149dd
Yehor Radchenko <y.p.radchenko@gmail.com>
2024-02-15 14:19:06 +0200

    Repository

---
+++ BookRate/DAL/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿
+using BookRate.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookRate.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T>, IDisposable where T : class
+    {
+        private readonly BookRateDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(BookRateDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
